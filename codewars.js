0e2c7b198382309361081ba8fd0ec8cb48689bbd
Roman Morozenko <romanvm2020@gmail.com>
2022-05-18 01:40:28 +0300

    add Square Every Digit solution

--- codewars.js
+++ codewars.js
@@ -56,6 +56,18 @@ function getCount(str) {
     return vowelsCount;
   }
 
-  // try to solve with regex
+  // ^^^^^^ try to solve with regex ^^^^^^
 
-  
+
+
+//           Square Every Digit
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+function squareDigits(num){
+    return +[...num+''].map(el=>parseInt(el)**2).join('');
+  }
