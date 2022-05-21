97a733f397d4ddbeafc55f75ca299b293fce31af
Roman Morozenko <romanvm2020@gmail.com>
2022-05-21 19:05:02 +0300

    add solution Is the date today

--- codewars.js
+++ codewars.js
@@ -91,3 +91,20 @@ String.prototype.toJadenCase = function () {
     .join(' ')           
   };
 
+
+
+
+// Is the date today
+
+// Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+
+// Make sure that your function does not return a false positive by only checking the day.
+
+function isToday(date) {
+
+    return new Date().toDateString() === date.toDateString();
+
+  }
+
+
+
