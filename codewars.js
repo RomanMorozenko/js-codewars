1268417768543e92737cb065832c96c921b0aca2
Roman Morozenko <romanvm2020@gmail.com>
2022-05-27 02:46:34 +0300

    added solutions

--- codewars.js
+++ codewars.js
@@ -306,3 +306,76 @@ function sometimes(fn){
     }   
 }
 
+
+
+
+// Add the isUpperCase method to String to see whether the string is ALL CAPS. For example:
+
+// "c".isUpperCase() == false
+// "C".isUpperCase() == true
+// "hello I AM DONALD".isUpperCase() == false
+// "HELLO I AM DONALD".isUpperCase() == true
+// "ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase() == false
+// "ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase() == true
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function() {
+  if (this.length == 0) return true;
+  let myStr = JSON.stringify(this)
+  if (myStr.toUpperCase() == myStr) return true;
+  return false 
+}
+
+
+
+//  Array Helpers
+
+// This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// square() must return a copy of the array, containing all values squared
+// cube() must return a copy of the array, containing all values cubed
+// average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+// sum() must return the sum of all array values
+// even() must return an array of all even numbers
+// odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+// Example
+var numbers = [1, 2, 3, 4, 5];
+
+// numbers.square();  // must return [1, 4, 9, 16, 25]
+// numbers.cube();    // must return [1, 8, 27, 64, 125]
+// numbers.average(); // must return 3
+// numbers.sum();     // must return 15
+// numbers.even();    // must return [2, 4]
+// numbers.odd();     // must return [1, 3, 5]
+
+Array.prototype.square = function() {
+  return this.map(item=> item*=item)
+}
+Array.prototype.cube = function() {
+  return this.map(item=> item**3)
+}
+Array.prototype.average = function() {
+ return  this.length == 0 
+         ? NaN 
+         : (this.reduce((current,item)=>current+=item,0))/this.length
+}
+Array.prototype.sum = function() {
+  return this.reduce((current,item)=>current+=item,0)
+}
+Array.prototype.even = function() {
+   return this.filter(item=> item%2 == 0)
+}
+Array.prototype.odd = function() {
+  return this.filter(item=> item%2 != 0)
+}
+
+console.log(numbers.square());  // must return [1, 4, 9, 16, 25]
+console.log(numbers.cube());    // must return [1, 8, 27, 64, 125]
+console.log(numbers.average()); // must return 3
+console.log(numbers.sum());     // must return 15
+console.log(numbers.even());    // must return [2, 4]
+console.log(numbers.odd());     // must return [1, 3, 5]
